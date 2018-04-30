cdc71990d1aea7d607a02acb6c6d38116d333da9
jochen.garcke <jochen@garcke.de>
2018-04-30 13:39:09 +0200

    fix admin-part, config

--- templatesadmin/admin.py
+++ templatesadmin/admin.py
@@ -1,54 +1,54 @@
 from django.contrib import admin
 from django.conf.urls import url
-from django.contrib.admin.sites import AdminSite
 
 from django.utils.translation import ugettext as _
-from django.http import HttpResponseRedirect, HttpResponseForbidden , HttpResponse
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 
-from templatesadmin.models import FTemplate 
-from templatesadmin.views import overview, edit
+from templatesadmin.models import FTemplate
+from templatesadmin.views import listing, modify
 
 from functools import update_wrapper
+
 
 class TemplatesAdmin(admin.ModelAdmin):
     """
         Admin for TemplatesAdmin
     """
-    model = FTemplate 
+    model = FTemplate
 
     def add_view(self, request, form_url='', extra_content=None):
-        return HttpResponseForbidden( _("You can't create new template's by admin interface. Try using the console.") )
+        return HttpResponseForbidden(_("You can't create new template's by admin interface. Try using the console."))
 
-    def delete_view(self, request, object_id, extra_context=None ):
-        return HttpResponseForbidden( _("You can't delete templates by admin. Try using the console.") )
-        pass
+    def delete_view(self, request, object_id, extra_context=None):
+        return HttpResponseForbidden(_("You can't delete templates by admin. Try using the console."))
 
-    def history_view(self, request, object_id , extra_context=None):
+    def history_view(self, request, object_id, extra_context=None):
         return HttpResponseRedirect('/')
 
-    def changelist_view(self, request ): 
-        return overview(request)
+    def changelist_view(self, request):
+        return listing(request)
 
     def change_view(self, request, object_id, extra_context=None):
-        return edit(request, object_id ) 
+        return modify(request, object_id)
 
     def get_urls(self):
         """
-            Get templatesadmin admin urls 
+            Get templatesadmin admin urls
         """
 
         def wrap(view):
-            def wrapper(*args,**kwargs):
+            def wrapper(*args, **kwargs):
                 return self.admin_site.admin_view(view)(*args, **kwargs)
             return update_wrapper(wrapper, view)
 
-        # This is copied on admin.py. 
+        # This is copied on admin.py.
         urlpatterns = [
-            url(r'^$',                          wrap(self.changelist_view),     name='templatesadmin_ftemplate_changelist'), 
-            url(r'^add/',                        wrap(self.add_view),            name='templatesadmin_ftemplate_add'), 
-            url(r'^edit/(?P<object_id>.*)/$',    wrap(self.change_view),         name='templatesadmin_ftemplate_change'), 
+            url(r'^$',                           wrap(self.changelist_view),     name='templatesadmin_ftemplate_changelist'),
+            url(r'^add/',                        wrap(self.add_view),            name='templatesadmin_ftemplate_add'),
+            url(r'^edit/(?P<object_id>.*)/$',    wrap(self.change_view),         name='templatesadmin_ftemplate_change'),
         ]
 
         return urlpatterns
+
 
 admin.site.register(FTemplate, TemplatesAdmin)
