e09ef6fb5b6426b06bc76079735972493cad6f04
jochen.garcke <jochen@garcke.de>
2018-04-30 13:14:29 +0200

    update to django 1.11, update CodeMirror

--- templatesadmin/forms.py
+++ templatesadmin/forms.py
@@ -1,38 +1,37 @@
 from django import forms
-from django.forms.forms import DeclarativeFieldsMetaclass 
-from widgets import CodeMirrorEditor
+from .widgets import CodeMirrorEditor
+
 
 class TemplateForm(forms.Form):
     content = forms.CharField(
-        widget = forms.Textarea(attrs={'rows': 10, 'cols': 40})
+      widget=forms.Textarea(attrs={'rows': 10, 'cols': 80})
     )
 
     def __init__(self, *args, **kwargs):
         """
             Backward compatibility for RichTemplateForm
         """
-        kwargs.pop('widget_syntax')
+        kwargs.pop('widget_extension')
 
-        super(TemplateForm, self).__init__( *args, **kwargs)
-        print self.fields
+        super(TemplateForm, self).__init__(*args, **kwargs)
 
 
-class RichTemplateForm( forms.Form ):
+class RichTemplateForm(forms.Form):
     """
         Display the code using CodeMirror editor
     """
     content = forms.CharField(
-        widget = forms.Textarea(attrs={'rows': 10, 'cols': 40})
+      widget=forms.Textarea(attrs={'rows': 10, 'cols': 80})
     )
 
     def __init__(self, *args, **kwargs):
-        syntax = kwargs.pop('widget_syntax') or 'dummy' 
+        extension = kwargs.pop('widget_extension')
 
-        super(RichTemplateForm, self).__init__( *args, **kwargs )
+        super(RichTemplateForm, self).__init__(*args, **kwargs)
 
         #
-        # Overwrite editor field dynamically 
-        # 
-        self.fields['content'] =  forms.CharField(
-            widget = CodeMirrorEditor(attrs={'syntax': syntax})
-        ) 
+        # Overwrite editor field dynamically
+        #
+        self.fields['content'] = forms.CharField(
+            widget=CodeMirrorEditor(attrs={'extension': extension})
+        )
